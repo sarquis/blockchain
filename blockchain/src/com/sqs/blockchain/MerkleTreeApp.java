050e2bead7f265020d4596f6dac38cd7641cff88
rsnf <roberto.sarquis@gmail.com>
2024-01-16 22:13:55 -0300

    Merkle Tree

---
+++ blockchain/src/com/sqs/blockchain/MerkleTreeApp.java
@@ -0,0 +1,51 @@
+package com.sqs.blockchain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MerkleTreeApp {
+
+    private List<String> transactions;
+
+    private MerkleTreeApp(List<String> transactions) {
+	this.transactions = transactions;
+    }
+
+    private String getMerkleRoot() {
+	return construct(transactions).get(0);
+    }
+
+    private List<String> construct(List<String> transactions) {
+	if (transactions.size() == 1)
+	    return transactions;
+
+	List<String> updatedList = new ArrayList<>();
+
+	for (int i = 0; i < transactions.size() - 1; i += 2)
+	    updatedList.add(mergeHash(transactions.get(i), transactions.get(i + 1)));
+
+	// If the number of transactions is odd. (Hc + Hc)
+	if (transactions.size() % 2 == 1)
+	    updatedList.add(mergeHash(
+		    transactions.get(transactions.size() - 1),
+		    transactions.get(transactions.size() - 1)));
+
+	return construct(updatedList);
+    }
+
+    private String mergeHash(String hash1, String hash2) {
+	String mergedHash = hash1 + hash2;
+	return SHA256Helper.generateHash(mergedHash);
+    }
+
+    public static void main(String[] args) {
+	List<String> transactions = new ArrayList<>();
+	transactions.add("aa");
+	transactions.add("bb");
+	transactions.add("cc");
+
+	MerkleTreeApp merkleTreeApp = new MerkleTreeApp(transactions);
+	System.out.println(merkleTreeApp.getMerkleRoot());
+    }
+
+}
